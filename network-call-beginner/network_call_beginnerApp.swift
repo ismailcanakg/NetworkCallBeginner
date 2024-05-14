f3b8c85c4fc3fe7ba4a7115ddbe77e9b438b4283
İsmail Can Akgün <ismailcanakg1@gmail.com>
2024-05-14 19:33:46 +0300

    Initial Commit

---
+++ network-call-beginner/network_call_beginnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  network_call_beginnerApp.swift
+//  network-call-beginner
+//
+//  Created by İsmail Can Akgün on 14.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct network_call_beginnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
